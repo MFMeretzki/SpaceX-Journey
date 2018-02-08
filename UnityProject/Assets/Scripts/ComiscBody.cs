4c9b545c740ee728f28ba12cb62f9ac0f387236a
David FM <mfdivad@gmail.com>
2018-02-08 10:15:26 +0100

    Created the CosmicBody class.

---
+++ UnityProject/Assets/Scripts/ComiscBody.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ComiscBody : MonoBehaviour {
+
+	private Rigidbody2D rigidbody2d;
+	private Vector2 externalForces;
+	
+	void Start ()
+	{
+	}
+	
+	void Update ()
+	{
+		rigidbody2d.AddForce(externalForces);
+		externalForces = Vector2.zero;
+	}
+
+	public void AddForce (Vector2 force)
+	{
+		externalForces += force;
+	}
+}
