48ae156b20a2c8646471d7095345d56abc114f61
Micael Fernandez Meretzki <micael.fernandez.tic@gmail.com>
2018-02-08 20:07:43 +0100

    Stupid change

--- UnityProject/Assets/Scripts/InputController.cs
+++ UnityProject/Assets/Scripts/InputController.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 
-public abstract class InputController : MonoBehaviour {
-
+public abstract class InputController : MonoBehaviour 
+{
 	public abstract bool ThrustersBurning ();
 
 	public abstract bool ChangeDirection ();
 	public abstract Vector2 GetDirection ();
-
 }
