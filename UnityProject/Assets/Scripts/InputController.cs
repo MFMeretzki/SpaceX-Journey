902c4f7f7b8c8cb45f07bf6e5519645f07a16e5d
David FM <mfdivad@gmail.com>
2018-02-08 17:15:51 +0100

    Implemented the basic functionalities of CosmicBody and Spaceship
    Implemented basic CameraController and InputController/StandardInput.

---
+++ UnityProject/Assets/Scripts/InputController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public abstract class InputController : MonoBehaviour {
+
+	public abstract bool ThrustersBurning ();
+
+	public abstract bool ChangeDirection ();
+	public abstract Vector2 GetDirection ();
+
+}
---
+++ UnityProject/Assets/Scripts/Test/PlanetGravityTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravityTest : MonoBehaviour {
+
+	public CosmicBody asteroid;
+	public float gravity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = transform.position - asteroid.transform.position;
+		direction.Normalize();
+		asteroid.AddForce(direction * gravity);
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+	}
+}
