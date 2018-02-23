204aa81ff57aab0afc90a2005efc394ef72d3b99
Micael Fernandez Meretzki <micael.fernandez.tic@gmail.com>
2018-02-23 15:14:27 +0100

    Now if a record is saved without specifying name it is saved with the placeholder

--- UnityProject/Assets/Scripts/UI/GameOverScreen.cs
+++ UnityProject/Assets/Scripts/UI/GameOverScreen.cs
@@ -6,6 +6,7 @@ public class GameOverScreen : MonoBehaviour {
 
     const string RESTART_ID = "gameover_restart";
     const string MAIN_MENU_ID = "gameover_main_menu";
+    const string PLACEHOLDER_ID = "gameover_name_placeholder";
     const string OUT_OF_FUEL_ID = "gameover_out_of_fuel";
     const string SHIP_DESTROYED_ID = "gameover_ship_destroyed";
 
@@ -29,17 +30,17 @@ public class GameOverScreen : MonoBehaviour {
     GameObject inputField;
     [SerializeField]
     Text inputText;
+    [SerializeField]
+    Text placeholderText;
 
-    bool isNewRecord;
+    bool isNewRecord = true;
     int points;
 
     private void OnEnable ()
     {
         restart.text = Localization.Instance.GetText(RESTART_ID);
         mainMenu.text = Localization.Instance.GetText(MAIN_MENU_ID);
-        sticker.enabled = false;
-        inputField.SetActive(false);
-        isNewRecord = false;
+        placeholderText.text = string.Format("{0}...", Localization.Instance.GetText(PLACEHOLDER_ID));
     }
 
     void Start () { }
@@ -53,11 +54,11 @@ public class GameOverScreen : MonoBehaviour {
         points = ore;
         score.text = points.ToString();
 
-        if (GameSettings.Instance.IsNewRecord(points) != -1)
+        if (GameSettings.Instance.IsNewRecord(points) == -1)
         {
-            sticker.enabled = true;
-            inputField.SetActive(true);
-            isNewRecord = true;
+            sticker.enabled = false;
+            inputField.SetActive(false);
+            isNewRecord = false;
         }
     }
 
@@ -94,7 +95,9 @@ public class GameOverScreen : MonoBehaviour {
     {
         if (isNewRecord)
         {
-            GameSettings.Instance.AddRecord(inputText.text, points);
+            string txt = 
+                (inputText.text.Length != 0) ? inputText.text : Localization.Instance.GetText(PLACEHOLDER_ID);
+            GameSettings.Instance.AddRecord(txt, points);
         }
     }
 }
