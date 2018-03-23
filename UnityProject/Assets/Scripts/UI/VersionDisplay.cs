a089d8409078de9403db5a9f88f0f157e476bee0
David FM <mfdivad@gmail.com>
2018-03-23 19:10:53 +0100

    The developement version shows on screen now.

---
+++ UnityProject/Assets/Scripts/UI/VersionDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionDisplay : MonoBehaviour {
+
+	public Text versionText;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+		versionText.text = "Developement version " + Application.version;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
